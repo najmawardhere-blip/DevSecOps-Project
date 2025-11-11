0449c0d4931d2e5cb74ebcade005d024391262dd
najmawardhere-blip <najma.wardhere@gmail.com>
2025-11-11 14:02:53 +0000

    update

--- main.tf
+++ main.tf
@@ -1,6 +1,41 @@
-provider "aws" {
-  region = "eu-west-2" # London region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.64.0"
+    }
+  }
 }
-resource "aws_s3_bucket" "terraformbuckettodaysun" {
-  bucket = "terraformbuckettodaysun"
+
+#provider "aws" {
+#  profile = "live"
+#  region  = "eu-west-2"
+#}
+
+provider "aws" {
+  alias   = "dev"
+  profile = "dev"
+  region  = "eu-west-2"
+}
+
+provider "aws" {
+  alias   = "live"
+  profile = "live"
+  region  = "eu-west-2"
+}
+
+module "live_state" {
+  source = "./state"
+
+  providers = {
+    aws = aws.live
+  }
+}
+
+module "dev_state" {
+  source = "./state"
+
+  providers = {
+    aws = aws.dev
+  }
 }
