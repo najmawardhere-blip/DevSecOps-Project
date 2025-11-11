0738a443fd296b022695bda8bea989b1497d2a57
najmawardhere-blip <najma.wardhere@gmail.com>
2025-11-11 14:40:54 +0000

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "terraformbuckettodaysun" {
+  bucket = "terraformbuckettodaysun"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraformbuckettodaysun"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
