2b458503e8302f68400f27a065bcad19e54700c8
najma wardhere <najma.wardheregmail.com>
2025-11-25 15:27:35 +0000

    update

--- main.tf
+++ main.tf
@@ -3,5 +3,12 @@ provider "aws" {
 }
 resource "aws_s3_bucket" "terraformbuckettodaysun" {
   bucket = "terraformbuckettodaysun"  
-   block_public_acls = true
+}
+resource "aws_s3_bucket_public_access_block" "terraformbuckettodaysun_block" {
+  bucket = aws_s3_bucket.terraformbuckettodaysun.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
