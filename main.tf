8c22e7c9089950f08661f6fd11eb054878162e76
najma wardhere <najma.wardheregmail.com>
2025-11-25 14:28:36 +0000

    update

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region = "eu-west-2" # London region
 }
 resource "aws_s3_bucket" "terraformbuckettodaysun" {
-  bucket = "terraformbuckettodaysun"
-  
+  bucket = "terraformbuckettodaysun"  
 }
  
